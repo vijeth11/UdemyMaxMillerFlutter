7a8c4f60558149d2547bcd946790fcb9ce13856c
Vijeth Belle <vijeth11@yahoo.co.in>
2022-07-29 11:50:26 -0400

    added effects for coin

--- simple_platform_game/lib/game/actor/coin.dart
+++ simple_platform_game/lib/game/actor/coin.dart
@@ -1,9 +1,13 @@
 import 'dart:ui';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flutter/animation.dart';
+import 'package:simple_platform/game/actor/player.dart';
 
-class Coin extends SpriteComponent{
-   Coin(
+class Coin extends SpriteComponent with CollisionCallbacks {
+  Coin(
     Image image, {
     Paint? paint,
     Vector2? position,
@@ -13,7 +17,7 @@ class Coin extends SpriteComponent{
     Anchor? anchor,
     int? priority,
   }) : super.fromImage(image,
-            srcPosition: Vector2(3*32,0),
+            srcPosition: Vector2(3 * 32, 0),
             srcSize: Vector2.all(32),
             position: position,
             paint: paint,
@@ -22,4 +26,26 @@ class Coin extends SpriteComponent{
             angle: angle,
             anchor: anchor,
             priority: priority);
+
+  @override
+  Future<void>? onLoad() {
+    add(CircleHitbox()..collisionType = CollisionType.passive);
+    add(MoveEffect.by(
+        Vector2(0, -10),
+        EffectController(
+            alternate: true, infinite: true, duration: 1, curve: Curves.ease)));
+    return super.onLoad();
+  }
+
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Player) {
+      add(OpacityEffect.fadeOut(LinearEffectController(0.3))
+        ..onComplete = () {
+          add(RemoveEffect());
+        });
+    }
+    super.onCollisionStart(intersectionPoints, other);
+  }
 }
