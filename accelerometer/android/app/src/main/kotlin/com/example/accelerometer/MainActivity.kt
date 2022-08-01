d0619832b70d7b356ddcd41c43f854bd9b56eaa1
Vijeth Belle <vijeth11@yahoo.co.in>
2022-08-01 14:38:04 -0400

    changed the app name

---
+++ accelerometer/android/app/src/main/kotlin/com/example/accelerometer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.accelerometer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ping_pong/android/app/src/main/kotlin/com/example/ping_pong/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ping_pong
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
