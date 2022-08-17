ba7a88d3ff6e154933cf3b97753a8e48a7f0beba
Vijeth Belle <vijeth11@yahoo.co.in>
2022-08-17 10:13:34 -0400

    added animation and increased the size of game play

--- leena/lib/actors/leena.dart
+++ leena/lib/actors/leena.dart
@@ -1,13 +1,21 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:leena/main.dart';
 import 'package:leena/world/ground.dart';
 
-class Leena extends SpriteComponent
+class Leena extends SpriteAnimationComponent
     with CollisionCallbacks, HasGameRef<LeenaGame> {
   bool onGround = false;
   bool facingRight = true;
+  final Vector2 sizeOfImage = Vector2(170, 222);
+  final double animationSpeed = 0.1;
+  late SpriteAnimation moving;
+  late SpriteAnimation onePush;
+  late SpriteAnimation jump;
+  late SpriteAnimation ideal;
   Leena() : super() {
     debugMode = true;
   }
@@ -25,6 +33,24 @@ class Leena extends SpriteComponent
       ..renderShape = false
       ..paint = (Paint()..color = Colors.red));
     anchor = Anchor.bottomCenter;
+
+    var movingSheet = SpriteSheet(
+        image: Flame.images.fromCache('moving.png'), srcSize: sizeOfImage);
+    moving = movingSheet.createAnimation(
+        row: 0, stepTime: animationSpeed, to: 19, loop: true);
+    var onePushSheet = SpriteSheet(
+        image: Flame.images.fromCache('onePush.png'), srcSize: sizeOfImage);
+    onePush = onePushSheet.createAnimation(
+        row: 0, stepTime: 0.1, to: 19, loop: false)
+      ..onComplete = () => animation = moving;
+    var jumpSheet = SpriteSheet(
+        image: Flame.images.fromCache('jump.png'), srcSize: sizeOfImage);
+    jump = jumpSheet.createAnimation(row: 0, stepTime: 0.1, to: 12, loop: true);
+    var idealSheet = SpriteSheet(
+        image: Flame.images.fromCache('girl.png'), srcSize: sizeOfImage);
+    ideal =
+        idealSheet.createAnimation(row: 0, stepTime: 0.1, to: 1, loop: true);
+    animation = ideal;
     return super.onLoad();
   }
 
@@ -32,6 +58,43 @@ class Leena extends SpriteComponent
   void update(double dt) {
     if (!onGround) {
       gameRef.velocity.y += gameRef.gravity;
+    }
+    // check if leena is inside boundry before adding velocity
+    if (x > 0 && x - width < gameRef.mapWidth) {
+      if (onGround) {
+        // going right
+        if (facingRight) {
+          if (gameRef.velocity.x < 0) {
+            gameRef.velocity.x += gameRef.groundFriction;
+          } else if (gameRef.velocity.x > 0) {
+            gameRef.velocity.x -= gameRef.groundFriction;
+            // stop leena once velocity is reached 0 by friction
+            if (gameRef.velocity.x < 0) {
+              gameRef.velocity.x = 0;
+            }
+          }
+        }
+        // going left
+        else {
+          if (gameRef.velocity.x > 0) {
+            gameRef.velocity.x -= gameRef.groundFriction;
+          } else if (gameRef.velocity.x < 0) {
+            gameRef.velocity.x += gameRef.groundFriction;
+            // stop leena once velocity is reached 0 by friction
+            if (gameRef.velocity.x > 0) {
+              gameRef.velocity.x = 0;
+              animation = ideal;
+            }
+          }
+        }
+      }
+    } else {
+      gameRef.velocity.x = 0;
+      // bounce back so leena can move again
+      x += x <= 0 ? width : -1.5*width;
+    }
+    if (gameRef.velocity.x == 0) {
+      animation = ideal;
     }
     position += gameRef.velocity * dt;
     super.update(dt);
@@ -41,18 +104,19 @@ class Leena extends SpriteComponent
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     if (other is Ground) {
       gameRef.velocity.y = 0;
-      // Bounce back a bit and not go into the ground
-      if (!onGround && y - intersectionPoints.last[1] > 3) {
+      // Bounce back a bit and not go into the ground. Check if the ground is not too high it can be side wall
+      if (!onGround && y - intersectionPoints.last[1] > 3 && y - intersectionPoints.last[1] < 20) {
         y -= y - intersectionPoints.last[1] - 3;
+        animation = gameRef.velocity.x == 0 ? ideal : moving;
         onGround = true;
       }
       if (gameRef.velocity.x != 0) {
         for (var point in intersectionPoints) {
           if (y - 20 >= point[1]) {
             print("Hit the wall");
-            gameRef.velocity.x = facingRight ? -5 : 5;
+            gameRef.velocity.x = facingRight ? -10 : 10;
+            animation = ideal;
           }
-          print(point);
         }
       }
     }
--- leena/lib/main.dart
+++ leena/lib/main.dart
@@ -18,61 +18,77 @@ Future<void> main() async {
 class LeenaGame extends FlameGame with HasCollisionDetection, TapDetector {
   Leena leena = Leena();
   final double gravity = 3.0;
-  final double pushSpeed = 100;
+  final double pushSpeed = 80;
   final double jumpForce = 80;
+  final double groundFriction = 1.0;
   Vector2 velocity = Vector2(0, 0);
   late TiledComponent homeMap;
+  late double mapWidth;
+  late double mapHeight;
 
   @override
   Future<void>? onLoad() async {
-    await Flame.images.load('girl.png');
+    await Flame.images
+        .loadAll(['girl.png', 'moving.png', 'onePush.png', 'jump.png']);
     homeMap = await TiledComponent.load('map.tmx', Vector2.all(32));
-    double mapWidth = homeMap.tileMap.map.width.toDouble() * 32;
-    double mapHeight = homeMap.tileMap.map.height.toDouble() * 32;
-    double viewportWidth = mapWidth;
-    double viewportHeight = mapWidth * (size.y / size.x);
+    mapWidth = homeMap.tileMap.map.width.toDouble() * 32;
+    mapHeight = homeMap.tileMap.map.height.toDouble() * 32;
+    double viewportWidth = (mapHeight * (size.x / size.y)).ceil().toDouble();
+    double viewportHeight = mapHeight;
+    print("$viewportWidth width $viewportHeight height ");
     // to get the below screen resolution match it with ratio of the screen
     // for ex: current screen size is 801 / 392 which is almost 2:1
-    camera.viewport = FixedResolutionViewport(Vector2(mapWidth, mapHeight));
+    //camera.viewport = FixedResolutionViewport(Vector2(mapWidth, mapHeight));
+    camera.viewport =
+        FixedResolutionViewport(Vector2(viewportWidth, viewportHeight));
     add(homeMap);
     final ObstacleGroup = homeMap.tileMap.getLayer<ObjectGroup>('ground');
 
     for (var obj in ObstacleGroup!.objects) {
       add(Ground(
-          size: Vector2(obj.width, obj.width),
+          size: Vector2(obj.width, obj.height),
           position: Vector2(obj.x, obj.y)));
     }
 
     leena
-      ..sprite = Sprite(Flame.images.fromCache('girl.png'))
-      ..size = Vector2(170, 220)
-      ..position = Vector2(200, 30);
+      ..size = Vector2(100, 129)
+      ..position = Vector2(230, 30);
     add(leena);
+    camera.followComponent(leena,
+        worldBounds: Rect.fromLTRB(0, 0, mapWidth, mapHeight));
     return super.onLoad();
   }
 
   @override
   void onTapDown(TapDownInfo info) {
     if (leena.onGround) {
-      if (info.eventPosition.game.x < 100) {
+      if (info.eventPosition.viewport.x < 100) {
         print('left tap down');
         if (leena.facingRight) {
           leena.flipHorizontallyAroundCenter();
           leena.facingRight = false;
         }
+        velocity.x =0;
         velocity.x -= pushSpeed;
-      } else if (info.eventPosition.game.x > size.x - 100) {
+        leena.onePush.reset();
+        leena.animation = leena.onePush;
+      } else if (info.eventPosition.viewport.x > size.x - 100) {
         print('right tap down');
         if (!leena.facingRight) {
           leena.flipHorizontallyAroundCenter();
           leena.facingRight = true;
         }
+        velocity.x =0;
         velocity.x += pushSpeed;
+        leena.onePush.reset();
+        leena.animation = leena.onePush;
       }
       if (info.eventPosition.game.y < 100) {
         print('jump');
         leena.y -= 10;
         velocity.y = -jumpForce;
+        leena.jump.reset();
+        leena.animation = leena.jump;
       }
     }
     super.onTapDown(info);
