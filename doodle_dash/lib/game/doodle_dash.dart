b726c38a6cc371acd03099cdcc103c103b628299
Vijeth Belle <vijeth11@yahoo.co.in>
2023-01-22 22:51:16 +0530

    updated the doodle

--- doodle_dash/lib/game/doodle_dash.dart
+++ doodle_dash/lib/game/doodle_dash.dart
@@ -1,8 +1,14 @@
+import 'package:doodle_dash/game/platform_manager.dart';
 import 'package:doodle_dash/game/world.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 
-class DoodleDash extends FlameGame {
+class DoodleDash extends FlameGame
+    with HasKeyboardHandlerComponents, HasCollisionDetection {
   final World _world = World();
+  final PlatformManager platfrormManager = PlatformManager(
+    maxVerticalDistanceToNextPlatform: 350
+  );
 
   @override
   Future<void> onLoad() async {
--- doodle_dash/lib/game/platform_manager.dart
+++ doodle_dash/lib/game/platform_manager.dart
@@ -7,4 +7,52 @@ import 'package:flame/components.dart';
 class PlatformManager extends Component with HasGameRef<DoodleDash> {
   final Random random = Random();
   final List<Platform> platforms = [];
+  // This will depend on the player jump speed and screen width
+  final double maxVerticalDistanceToNextPlatform;
+
+  // Adjust this value change game difficulty
+  final double minVerticalDistanceToNextPlatform = 200;
+
+  PlatformManager({required this.maxVerticalDistanceToNextPlatform}) : super();
+
+  @override
+  void onMount() {
+    // the first platform will always be in the bottom third of the initial screen
+    var currentY =
+        gameRef.size.y - (random.nextInt(gameRef.size.y.floor()) / 3);
+
+    // Generate 10 platforms at random x, y positions and add to list of platforms
+    for (int i = 0; i < 9; i++) {
+      if (i != 0) {
+        currentY = _generateNextY();
+      }
+      platforms.add(Platform(
+          position: Vector2(
+              random.nextInt(gameRef.size.x.floor()).toDouble(), currentY)));
+    }
+    super.onMount();
+  }
+
+  // This method determines where the next platform should be placed
+  // It calculates a random distance between the minVerticalDistanceToNextPlatform
+  // and the maxVerticalDistanceToNextPlatform, and returns a Y coordinate that is
+  // that distance above the current highest platform
+
+  double _generateNextY() {
+    final currentHighestPlatform = platforms.last.center.y;
+    final distanceToNextY = minVerticalDistanceToNextPlatform.toInt() +
+        random
+            .nextInt((maxVerticalDistanceToNextPlatform -
+                    minVerticalDistanceToNextPlatform)
+                .toInt())
+            .toDouble();
+    return currentHighestPlatform - distanceToNextY;
+  }
+
+  @override
+  void update(double dt) {
+    final topOfLowestPlatform = platforms.first.position.y;
+
+    super.update(dt);
+  }
 }
